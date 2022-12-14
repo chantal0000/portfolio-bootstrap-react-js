705e6d59f23df3b080d4992a830bb0b7e3ea4962
Codingwithchantal <c_buerger@gmx.net>
2022-12-14 16:19:49 +0100

    position of nav to the right, sizing changed of nav items

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,22 @@
 import {NavbarBrand, Navbar, Nav, Container} from "react-bootstrap";
+import "./styles/navbar.css"
 
 // bootstrap navbar component
 function BtsNavbar() {
     return (<>
-        <Navbar bg="primary" expand="lg">
+        <Navbar bg='secondary' expand="md" >
             <Container>
-                <NavbarBrand>React Portfolio</NavbarBrand>
+                <NavbarBrand>Portfolio</NavbarBrand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav>
-                        <Nav.Item>
+                    <Nav className="ms-auto" bg="primary">
+                        <Nav.Item className="fs-5">
                             <Nav.Link href="/index.js">Home</Nav.Link>
                         </Nav.Item>
-                        <Nav.Item>
+                        <Nav.Item className="fs-5">
                             <Nav.Link href="/">Page 1</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
+                        </Nav.Item >
+                        <Nav.Item className="fs-5">
                             <Nav.Link href="/">Page 2</Nav.Link>
                         </Nav.Item>
                     </Nav>
