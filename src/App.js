d1ddd9cedcfb9e49d6429c8a301bdd5d74227c38
Codingwithchantal <c_buerger@gmx.net>
2022-12-14 16:46:07 +0100

    change color of nav-links when overed over them

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import BtsNavbar from "./components/navbar";
+
 
 
 function App() {
     return (<>
             <BtsNavbar></BtsNavbar>
+
             <div className="App">
                 <p className="">Hello World</p>
             </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,7 @@
 import {NavbarBrand, Navbar, Nav, Container} from "react-bootstrap";
 import "./styles/navbar.css"
 
+
 // bootstrap navbar component
 function BtsNavbar() {
     return (<>
@@ -11,13 +12,13 @@ function BtsNavbar() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto" bg="primary">
                         <Nav.Item className="fs-5">
-                            <Nav.Link href="/index.js">Home</Nav.Link>
+                            <Nav.Link className="nav-links" href="/index.js">Home</Nav.Link>
                         </Nav.Item>
                         <Nav.Item className="fs-5">
-                            <Nav.Link href="/">Page 1</Nav.Link>
+                            <Nav.Link className="nav-links" href="/">Page 1</Nav.Link>
                         </Nav.Item >
                         <Nav.Item className="fs-5">
-                            <Nav.Link href="/">Page 2</Nav.Link>
+                            <Nav.Link className="nav-links" href="">Contact</Nav.Link>
                         </Nav.Item>
                     </Nav>
                 </Navbar.Collapse>
