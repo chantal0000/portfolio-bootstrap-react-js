3b5b0ae98d5deb12b8cfc026b7d8da57c2b3bdfc
Codingwithchantal <c_buerger@gmx.net>
2022-12-19 13:29:57 +0100

    added styling to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,18 +6,21 @@ function MyPageFooter() {
     display: flex;
     flex-direction: row;
     gap: 20px;
+    //center text in footer
+    justify-content: center;
+    margin-top: 1rem;
     `;
 
 
     return (// jsx <> </>
         <>
             {/*section better for accessibility, when you browse with human assistant programs*/}
-            <section className="fixed-bottom bg-danger">
+            <section className="fixed-bottom bg-danger ">
                 <div className="MyFooter">
                     <div className="container">
                         <FlexComponent>
                             {/*copy right img*/}
-                            <p className="text-success">Chantal's Portfolio <span>&#169;</span></p>
+                            <p className="text-dark">Chantal's Portfolio <span>&#169;</span></p>
                             <br/>
                             <p className="text-muted">2022</p>
                         </FlexComponent>
