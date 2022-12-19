e1b02a366927d3fc72b4b4e347a9f99f9f713a71
Codingwithchantal <c_buerger@gmx.net>
2022-12-19 13:46:20 +0100

    changed height of body

---
+++ src/pages/NasaPhoto.js
@@ -0,0 +1,15 @@
+/* ! In order for this file to work, we need the following sections /
+/ ? Image Url - Image Title - Image Desc - Image Date/
+/? import UseState and UseEffect for toggling and fetching the Data and build the Html to display the image with basic html tags*/
+
+
+function NasaPhoto(){
+    return (
+        <>
+        <div>
+            <h1>NASA Photo</h1>
+        </div>
+        </>
+    )
+}
+export default NasaPhoto;
