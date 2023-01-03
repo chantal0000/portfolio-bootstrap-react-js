6cf07c8f8c8b1a614d2f6bca38e7108abb098e3a
Codingwithchantal <c_buerger@gmx.net>
2023-01-03 11:15:11 +0100

    delete pills from navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,24 +5,24 @@ import "./styles/navbar.css"
 function BtsNavbar() {
     return (<>
         {/*sticky-top navbar stays when you scroll*/}
-        <Navbar bg='secondary' expand="md" className="sticky-top">
+        <Navbar expand="md" className="sticky-top">
             <Container>
                 <NavbarBrand><a href="/">Portfolio</a></NavbarBrand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     {/*ms-auto for pos left*/}
-                    <Nav className="ms-auto" bg="primary" variant="pills" defaultActiveKey="/index.js">
+                    <Nav className="ms-auto" bg="primary">
                         <Nav.Item className="fs-5">
                             <Nav.Link className="nav-links" href="/index.js">Home</Nav.Link>
                         </Nav.Item>
                         <Nav.Item className="fs-5">
                             <Nav.Link
-                                href="/apiPlayground" className="nav-links" eventKey="/pages/apiPlayground.js">API
+                                href="/apiPlayground" className="nav-links">API
                                 Playground</Nav.Link>
                         </Nav.Item>
                         <Nav.Item className="fs-5">
                             <Nav.Link href="/contact" className="nav-links"
-                                      eventKey="/pages/contact.js">Contact</Nav.Link>
+                            >Contact</Nav.Link>
                         </Nav.Item>
                     </Nav>
                 </Navbar.Collapse>
