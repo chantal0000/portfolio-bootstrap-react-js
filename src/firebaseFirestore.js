00cbcd0c4fef155d277dd545f4942ad231e9f8ef
Codingwithchantal <c_buerger@gmx.net>
2023-01-26 12:14:21 +0100

    exchange upside down emjo svg

--- src/firebaseFirestore.js
+++ src/firebaseFirestore.js
@@ -2,12 +2,12 @@ import firebase from "firebase/compat/app";
 import 'firebase/compat/firestore'
 
 const firebaseConfig = {
-    apiKey:`${process.env.REACT_APP_API_KEY_FIREBASE}`,
-    authDomain:`${process.env.REACT_APP_AUTH_DOMAIN}`,
+    apiKey: `${process.env.REACT_APP_API_KEY_FIREBASE}`,
+    authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
     projectId: 'my-portfolio-react-f1cc2',
-    storageBucket:`${process.env.REACT_APP_STORAGE_BUCKET}`,
-    messagingSenderId:`${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
-    appId:`${process.env.REACT_APP_APP_ID}`
+    storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+    appId: `${process.env.REACT_APP_APP_ID}`
 };
 
 
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -11,16 +11,8 @@ function Homepage() {
                 <Col>
                     <div className="box">
                         <div className="Intro">
-                            <h1 className="display-1">Hello, I'm Chantal <svg id="rotate"
-                                                                              xmlns="http://www.w3.org/2000/svg"
-                                                                              width="16" height="16" fill="currentColor"
-                                                                              className="bi bi-emoji-smile-upside-down"
-                                                                              viewBox="0 0 16 16">
-                                <path d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zm0-1a8 8 0 1 1 0 16A8 8 0 0 1 8 0z"/>
-                                <path
-                                    d="M4.285 6.433a.5.5 0 0 0 .683-.183A3.498 3.498 0 0 1 8 4.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.498 4.498 0 0 0 8 3.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683zM7 9.5C7 8.672 6.552 8 6 8s-1 .672-1 1.5.448 1.5 1 1.5 1-.672 1-1.5zm4 0c0-.828-.448-1.5-1-1.5s-1 .672-1 1.5.448 1.5 1 1.5 1-.672 1-1.5z"/>
-                            </svg></h1>
-
+                            <h1 className="display-1">Hello, I'm Chantal <EmojiSmileUpsideDown className="bi"
+                                                                                               size={20}/></h1>
                         </div>
                         <div className="Intro-Text">
                             <p>I'm a web developer based in Berlin, Germany. Lorem ipsum dolor sit amet, consetetur
@@ -31,39 +23,33 @@ function Homepage() {
                                 duo
                                 dolores et ea rebum. </p>
                         </div>
-
-
                         {/*<div className="web-dev"><p>WEB DEVELOPER</p></div>*/}
                     </div>
                     {/*<div className="pronouns"><p>SHE/HER</p></div>*/}
                     <div className="check-out-projects">
                         <p>CHECK OUT MY PROJECTS</p>
                         <ArrowDown size={30}/></div>
-
                 </Col>
             </Row>
         </Container>
-            <Container fluid className="BannerContainer">
-                <Row className="BannerRow">
-                    {/*<Col className="MediaCol col-sm-6 col-md-4 col-lg-8">*/}
-                    <Col>
-                        <Container className="container">
-                            <Row className="header">
-                                <Col><h1>project collection</h1></Col>
-                                {/*<Col><p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt*/}
-                                {/*    ut labore et dolore magna aliquyam erat, sed diam voluptua. </p></Col>*/}
-                                <Col><p>check out my GitHub for all my work [button GH]</p></Col>
-                            </Row>
-                        </Container>
-                    </Col>
-                    {/*<Col className="BannerInfo col-sm-6 col-md-8 col-lg-4">*/}
-                    {/*    <p>Banner RIGHT</p>*/}
-                    {/*</Col>*/}
-                </Row>
+        <Container fluid className="BannerContainer">
+            <Row className="BannerRow">
+                {/*<Col className="MediaCol col-sm-6 col-md-4 col-lg-8">*/}
+                <Col>
+                    <Container className="container">
+                        <Row className="header">
+                            <Col><h1>project collection</h1></Col>
+                            <Col><p>check out my GitHub for all my work [button GH]</p></Col>
+                        </Row>
+                    </Container>
+                </Col>
+                {/*<Col className="BannerInfo col-sm-6 col-md-8 col-lg-4">*/}
+                {/*    <p>Banner RIGHT</p>*/}
+                {/*</Col>*/}
+            </Row>
 
 
-
-            </Container>
+        </Container>
         <Container fluid className="Education-Project p-5">
             <Row>
                 <Col><Projects></Projects></Col>
@@ -74,7 +60,7 @@ function Homepage() {
                 <Col><Projects></Projects></Col>
             </Row>
         </Container>
-        </>)
+    </>)
 }
 
 export default Homepage;
